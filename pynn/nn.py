b8604adac9b9b9727df6b21a49700fdbdb702813
Rocco Meli <r.meli@bluemail.ch>
2018-11-04 18:53:47 +0000

    neural network

---
+++ pynn/nn.py
@@ -0,0 +1,30 @@
+"""
+Neural network.
+
+A neural network is a sequential connection of layers. It has an input layer,
+a number of hidden layers and an output layer.
+"""
+
+from pynn.tensor import Tensor
+from pynn.layer import Layer
+
+from typing import Sequence
+
+class NeuralNetwork:
+
+    def __init__(self, layers: Sequence[Layer]) -> None:
+        self.layers = layers
+
+    def forward(self, inputs: Tensor) -> Tensor:
+        for layer in self.layers:
+            inputs = layer.forward(inputs)
+
+        return iunputs
+
+    def backward(self, grad: Tensor) -> Tensor:
+        for layer in reversed(self.layer):
+            grad = layer.backward(grad)
+
+        return grad
+
+    
