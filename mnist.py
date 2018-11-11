e51613488a2e3ac79bbf9df38fe997904c0e9db7
Rocco Meli <r.meli@bluemail.ch>
2018-11-11 23:49:40 +0000

    minst import

---
+++ mnist.py
@@ -0,0 +1,14 @@
+"""
+Train neuronal network to learn handwritten digits from the MNIST database.
+
+The MNIST database is composed of 8x8 images of handwritten digits.
+"""
+
+from matplotlib import pylab as plt
+from sklearn import datasets
+
+# Load MNIST database
+images, targets = datasets.load_digits(return_X_y=True)
+
+plt.imshow(images[0].reshape(8,8), cmap=plt.cm.gray_r, interpolation='nearest')
+plt.show()
